6bcb8e993d4a88f597437296c30d4ffe77118254
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-08-18 08:26:03 +0530

    Added dark theme toggle button using shadcn

---
+++ components/ToggleMode.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+
+const ToggleMode = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const dark = theme === "dark";
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Fix for initial error for dark theme loading
+  if (!mounted) {
+    return <Button variant="outline" size="icon" disabled={true}></Button>;
+  }
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(`${dark ? "light" : "dark"}`)}
+    >
+      {dark ? (
+        <Sun className="hover:cursor-pointer hover:text-primary" />
+      ) : (
+        <Moon className="hover:cursor-pointer hover:text-primary" />
+      )}
+    </Button>
+  );
+};
+
+export default ToggleMode;
