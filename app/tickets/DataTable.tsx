cc524117903e55ebd27f6a5f67874fb3a76a9c50
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-08-22 12:32:55 +0530

    Beautify STATUS column in table

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -1,3 +1,4 @@
+import TicketStatusBadge from "@/components/TicketStatusBadge";
 import {
   Table,
   TableBody,
@@ -21,7 +22,9 @@ const DataTable = ({ tickets }: Props) => {
           <TableHeader>
             <TableRow>
               <TableHead>Title</TableHead>
-              <TableHead>Status</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Status</div>
+              </TableHead>
               <TableHead>Priority</TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
@@ -31,7 +34,11 @@ const DataTable = ({ tickets }: Props) => {
               ? tickets.map((ticket) => (
                   <TableRow key={ticket.id} data-href="/">
                     <TableCell> {ticket.title} </TableCell>
-                    <TableCell> {ticket.status} </TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
                     <TableCell> {ticket.priority} </TableCell>
                     <TableCell>
                       {" "}
