24411630e9f4851f557cedf8bcad798546f60d04
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-08-22 11:35:25 +0530

    Little organized table created

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -16,32 +16,34 @@ interface Props {
 const DataTable = ({ tickets }: Props) => {
   //   console.log(tickets);
   return (
-    <div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Title</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Priority</TableHead>
-            <TableHead>Created At</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {tickets
-            ? tickets.map((ticket) => (
-                <TableRow key={ticket.id} data-href="/">
-                  <TableCell> {ticket.title} </TableCell>
-                  <TableCell> {ticket.status} </TableCell>
-                  <TableCell> {ticket.priority} </TableCell>
-                  <TableCell>
-                    {" "}
-                    {ticket.createdAt.toLocaleDateString()}{" "}
-                  </TableCell>
-                </TableRow>
-              ))
-            : null}
-        </TableBody>
-      </Table>
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Priority</TableHead>
+              <TableHead>Created At</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id} data-href="/">
+                    <TableCell> {ticket.title} </TableCell>
+                    <TableCell> {ticket.status} </TableCell>
+                    <TableCell> {ticket.priority} </TableCell>
+                    <TableCell>
+                      {" "}
+                      {ticket.createdAt.toLocaleDateString()}{" "}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
