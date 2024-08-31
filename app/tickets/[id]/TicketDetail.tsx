0e56b28e7c18ce8f68fbedc2bba7725d0fb1f6de
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-08-31 12:18:08 +0530

    Added shadcn card to display ticket details in shadcn/card

--- app/tickets/[id]/TicketDetail.tsx
+++ app/tickets/[id]/TicketDetail.tsx
@@ -1,16 +1,55 @@
 import { Ticket } from "@prisma/client";
 import React from "react";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import TicketPriority from "@/components/TicketPriority";
+
 interface Props {
   ticket: Ticket;
 }
 
 const TicketDetail = ({ ticket }: Props) => {
   return (
-    <div>
-      <p>{ticket.title}</p>
-      <p>{ticket.description}</p>
-    </div>
+    <Card>
+      <CardHeader>
+        <div className="flex justify-between mb-3">
+          <TicketStatusBadge status={ticket.status} />
+          <TicketPriority priority={ticket.priority} />
+        </div>
+        <CardTitle>{ticket.title}</CardTitle>
+        <CardDescription>
+          Created:{" "}
+          {ticket.createdAt.toLocaleDateString("en-US", {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+          })}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>{ticket.description}</CardContent>
+      <CardFooter>
+        Updated:{" "}
+        {ticket.updatedAt.toLocaleDateString("en-US", {
+          year: "2-digit",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: true,
+        })}
+      </CardFooter>
+    </Card>
   );
 };
 
