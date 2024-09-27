ac40683f8b40ee01dd09de0b5bbabd68130c9275
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-09-27 23:21:54 +0530

    Protected /users and /users/[id] endpoints

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,12 +1,19 @@
 import UserForm from "@/components/UserForm";
 import prisma from "@/prisma/db";
 import React from "react";
+import { getServerSession } from "next-auth";
+import options from "../../api/auth/[...nextauth]/options";
 
 interface Props {
   params: { id: string };
 }
 
 const EditUser = async ({ params }: Props) => {
+  const session = await getServerSession(options);
+  if (session?.user.role !== "ADMIN") {
+    return <p className="text-destructive">Admin access required</p>;
+  }
+
   const user = await prisma?.user.findUnique({
     where: { id: parseInt(params.id) },
   });
