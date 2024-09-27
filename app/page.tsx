4aeba9bc9d94dd10349a21941e01fe4ae2c78241
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-09-27 23:53:37 +0530

    Added Recent Tickets in Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+import DashChart from "@/components/DashChart";
+import DashRecentTickets from "@/components/DashRecentTickets";
+import prisma from "@/prisma/db";
+
+const Dashboard = async () => {
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT: [{ status: "CLOSED" }],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    skip: 0,
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const groupTicket = await prisma.ticket.groupBy({
+    by: ["status"],
+    _count: { id: true },
+  });
+  return (
+    <div>
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div>
+          <DashRecentTickets tickets={tickets} />
+        </div>
+        <div>
+          <DashChart />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
