5d0ebf379208e6945107945647008d0f445622f6
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-09-27 23:03:32 +0530

    Endpoints are now protected

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,13 +2,23 @@ import { userSchema } from "@/ValidationSchemas/users";
 import prisma from "@/prisma/db";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { getServerSession } from "next-auth";
+import options from "../auth/[...nextauth]/options";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(options);
+  if (!session) {
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+  }
+
   const body = await request.json();
   const validation = userSchema.safeParse(body);
 
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
   }
 
   const duplicate = await prisma.user.findUnique({
